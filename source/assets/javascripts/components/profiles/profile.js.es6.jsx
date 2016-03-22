f0de3dba5a0f4f9a0919a3d965f080675b70c4a7
Miguel Angel Gordian <os.aioria@gmail.com>
2016-03-22 04:17:04 -0600

    Agrega respuestas basadas en API
    Permite mostrar la información de organizaciones y componentes basandose en el API.

--- source/assets/javascripts/components/profiles/profile.js.es6.jsx
+++ source/assets/javascripts/components/profiles/profile.js.es6.jsx
@@ -1,67 +1,55 @@
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+  }
 
-    this.state = {
-      title: 'Semana de la seguridad Vial',
-      description: `Sugar plum candy canes candy marshmallow lemon drops
-      soufflé ice cream cheesecake fruitcake. Cookie sweet roll wafer sweet
-      cupcake. Marzipan fruitcake chocolate bar carrot cake wafer candy canes
-      chupa chups chocolate. Soufflé jujubes fruitcake.
-      Pudding muffin carrot cake ice cream cookie carrot cake.`,
-      label: 'Registrarme ->',
-      action: () => null,
-      components: {
-        title: '¿Cómo puedes participar?',
-        actions: [
-          {
-            title: 'NUlla malesuda',
-            description: 'Sugar plum candy canes candy marshmallow lemon drops',
-          },
-        ],
-      },
-      additional: {
-        title: 'Más Información',
-        description: `Sugar plum candy canes candy marshmallow lemon drops
-        soufflé ice cream cheesecake fruitcake.
-        Cookie sweet roll wafer sweet
+  componentWillMount() {
+    $.getJSON('https://script.google.com/a/macros/civica.digital/s/AKfycbyZBWk5JINK1ulRLfN8aZS8k9iMDp_1vIj2VKYhnRp-sMNbSleh/exec?resource=organizations')
+      .done((data) => {
+        const org = data.find(
+            (element) => element.id === parseInt(getUrlVars().organization));
 
-        cupcake. Marzipan fruitcake chocolate bar carrot cake wafer candy canes
-        chupa chups chocolate. Soufflé jujubes fruitcake.
-        Pudding muffin carrot cake ice cream cookie carrot cake.`,
-      },
-      organizations: {
-        title: 'Otras formas de participar',
-        orgs: [
-          {
-            id: 1,
-            organization_name: 'Academia Nacional de Arquitectura',
-            description: 'Gremio',
-            link: 'Mariangeles Martín',
-            contact: 'coordinacion@anamty.org ',
-            action_ids: [5],
-            ACV: true,
-            GCV: true,
-            logo: 'coordinacion@anamty.org',
-          },
-          {
-            id: 2,
-            organization_name: 'Adopta Monterrey',
-            description: 'Animalista',
-            link: 'Julieta Valdez',
-            contact: 'perrosadoptamonterrey@hotmail.com;',
-            action_ids: [6],
-            ACV: false,
-            GCV: true,
-            logo: 'perrosadoptamonterrey@hotmail.com;',
-          },
-        ],
-      },
-    };
+        const organizations = data.filter(
+          (data) => intersect(org.action_ids, data.action_ids).length !== 0
+        );
+
+        $.getJSON('https://script.google.com/a/macros/civica.digital/s/AKfycbyZBWk5JINK1ulRLfN8aZS8k9iMDp_1vIj2VKYhnRp-sMNbSleh/exec?resource=components')
+          .done((data) => {
+            const actions = data.filter(
+              (data) => org.action_ids.indexOf(data.id_accion) !== -1);
+
+            this.setState({
+              title: org.organization_name,
+              description: org.description,
+              label: 'Registrarme ->',
+              action: () => null,
+              additional: {
+                title: 'Más Información',
+                description: `Sugar plum candy canes candy marshmallow lemon drops
+                  soufflé ice cream cheesecake fruitcake.
+                  Cookie sweet roll wafer sweet
+
+                  cupcake. Marzipan fruitcake chocolate bar carrot cake wafer candy canes
+                  chupa chups chocolate. Soufflé jujubes fruitcake.
+                  Pudding muffin carrot cake ice cream cookie carrot cake.`,
+              },
+              organizations: {
+                title: 'Otras formas de participar',
+                orgs: organizations,
+              },
+              components: {
+                title: '¿Cómo puedes participar?',
+                actions: actions,
+              },
+            });
+          });
+      });
   }
 
   render() {
     const props = this.state;
+    if (props === null) return <div className='Profile' />;
+
     return <div className='Profile'>
       <Resume
         title={props.title}
